af9d1e481dccc5badbe14823b307e0d0525784b0
Josue Mosquera <josue.mosquera@101grados.com>
2025-01-20 15:47:42 -0500

    desktop header added

--- layouts/desktop/header/header.tsx
+++ layouts/desktop/header/header.tsx
@@ -31,7 +31,8 @@ export default function HeaderDesktop(props) {
       program.id === 13 ||
       program.id === 15 ||
       program.id === 14 ||
-      program.id === 26
+      program.id === 26 ||
+      program.id === 18
       ? true
       : false
   );
